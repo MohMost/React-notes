4bef5ec0792d9fe16d0d1a9cd2d395e16dc8ea9c
unknown <teemohmost2020@gmail.com>
2023-02-14 16:47:18 +0100

    :memo: (add): lesson 2.2 lifting stats

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,29 +1,15 @@
 import classes from './NewPost.module.css';
-import {useState} from 'react'
 
-
-function NewPost() {
-  //* useState 
-
-  const [enteredBody , setEnteredBody] = useState() //? enteredBody is the default state setEteredBody is the updated state an each time the setEnteredBody is set it will be stored as a new default value in enteredBody
-
-  //* Event listeners in react
-
-  //?step 1: setting up the function that will handle the event
-  function changeBodyHandler(e){
-    setEnteredBody(e.target.value)
-  }
+function NewPost(props) {
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-  {/*step 2: assigning the event listener and passing in its function (event={eventFunction})*/}
-        <textarea id="body" required rows={3} onChange={changeBodyHandler}/> 
+        <textarea id="body" required rows={3} onChange={props.onBodyChange}/>
       </p>
-      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={props.onAuthorChange} />
       </p>
     </form>
   );
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,14 +1,21 @@
 import Post from './Post';
 import NewPost from './NewPost'
 import classes from './PostsList.module.css';
-
+import {useState} from 'react'
 function PostsList() {
-  
+  const [enteredBody, setEnteredBody] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
+  function bodyChangeHandler(e) {
+    setEnteredBody(e.target.value)
+  }
+  function authorChangehandler(e) {
+    setEnteredAuthor(e.target.value)
+  }
   return (
    <>
-     <NewPost/>
+     <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangehandler}/>
      <ul className={classes.posts}>
-       <Post author="Mohmost" body="Welcome tomy world!" />
+       <Post author={enteredAuthor} body={enteredBody} />
        <Post author="Sofiane" body="Hey happy to see you !" />
      </ul>
    </>
